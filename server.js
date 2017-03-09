fe34a14c539fa86e00d3091032e57dafb873eb63
Rishabh Shukla <rishabhshukla25@hotmail.com>
2017-03-09 14:23:51 +0530

    setup server

--- server.js
+++ server.js
@@ -1,3 +1,15 @@
 /**
  * Created by rishabhshukla on 09/03/17.
  */
+
+
+const express = require("express");
+
+const app = express();
+
+app.use(express.static("static"));
+app.listen(3000, function(){
+
+    console.log("Listening on port 3000")
+
+});
