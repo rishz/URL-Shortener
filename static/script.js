745069322f05164d4113a581080fbbeaa6030449
Fardeen Khan <romanempirekk@gmail.com>
2021-01-08 17:13:01 +0530

    added changes in static/style.css
    added changes in static/style.css
    added changes in static/index.css

--- static/script.js
+++ static/script.js
@@ -7,7 +7,7 @@ $(function () {
         $.post('/api/v1/shorten', {
             url:url
         },function (data) {
-            $('#shortcode').html("Short URL: " + '<a href="/' + data + '">'+window.location.href+data + '</a>');
+            $('#shortcode').html("Shorting URL: " + '<a href="/' + data + '">'+window.location.href+data + '</a>');
         })
     })
 })
