11a3a9f7f2074312c9cf5ff9c883f1c3679eaa22
Ankur Bodhak <ankurbodhak69@gmail.com>
2021-05-06 21:06:45 +0530

    I have added new UI changes which can be only experienced by clicking the submit-button.

--- static/script.js
+++ static/script.js
@@ -4,10 +4,16 @@
 $(function () {
     $('#submit').click(function () {
         var url = $('#url').val();
+         var x = Math.floor(Math.random()*256);
+         var y = Math.floor(Math.random()*256);
+         var z = Math.floor(Math.random()*256);
+         var bgColor = "rgb(" + x + "," + y + "," + z + ")";
+         console.log(bgColor);
+         document.body.style.background = bgColor;
         $.post('/api/v1/shorten', {
             url:url
         },function (data) {
             $('#shortcode').html("Short URL: " + '<a href="/' + data + '">'+window.location.href+data + '</a>');
-        })
+        },)
     })
 })
