48884ca5a24cb6cc438c03bfbe2a732d5acc394e
yashnuhash <nuhashroxme@gmail.com>
2022-07-12 10:34:52 +0600

    New UI Changes Added

--- static/script.js
+++ static/script.js
@@ -7,7 +7,7 @@ $(function () {
         $.post('/api/v1/shorten', {
             url:url
         },function (data) {
-            $('#shortcode').html("Short URL: " + '<a href="/' + data + '">'+window.location.href+data + '</a>');
+            $('#shortcode').html("Your Short URL is Here: " + '<a href="/' + data + '">'+window.location.href+data + '</a>');
         })
     })
 })
